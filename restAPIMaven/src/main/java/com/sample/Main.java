44edda4d0eefe162d64158d946edfa038d6e8952
Oscar Molina <omolinab@redhat.com>
2015-01-22 18:27:00 +0100

    Updating restAPIMaven

---
+++ restAPIMaven/src/main/java/com/sample/Main.java
@@ -0,0 +1,139 @@
+package com.sample;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jbpm.services.task.utils.ContentMarshallerHelper;import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.process.ProcessInstance;
+import org.kie.api.task.TaskService;
+import org.kie.api.task.model.Content;
+import org.kie.api.task.model.Task;
+import org.kie.services.client.api.RemoteRestRuntimeEngineFactory;
+import org.kie.services.client.api.command.RemoteRuntimeEngine;
+
+/**
+ * This is a very simple Rest Client to test against a running instance of the business-central. You can parameterize - the
+ * Deployment Unit Id - the Application URL - the user/pass to execute operations
+ */
+public class Main {
+	
+	//set according to your environment
+	private static final String APP_URL="http://localhost:8080/business-central/";
+	private static final String USER = "bpmsAdmin";
+	private static final String PASSWORD = "admin@123";
+
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+
+
+		URL url = new URL(APP_URL);
+		
+		// create a factory for a given deploymentId
+		RemoteRestRuntimeEngineFactory factory = RemoteRestRuntimeEngineFactory.newBuilder()
+				.addDeploymentId("org.kie.example:project1:1.0").addUrl(url).addUserName(USER)
+				.addPassword(PASSWORD).build();
+
+		//startProcessWithCustomParam(factory, "project1.myprocess");
+		//humanTaskOutputWithCustomParam(factory, "project1.myprocess");
+		humanTaskInputWithCustomParam(factory, "project1.myprocess");
+
+	}
+
+	public static void startProcessWithCustomParam(RemoteRestRuntimeEngineFactory factory, String PROCESS_ID)
+			throws InterruptedException {
+
+		System.out.println("###########################################");
+		System.out.println("..testing Starting the Process with a Custom Param...");
+		System.out.println("###########################################");
+
+		RemoteRuntimeEngine engine = factory.newRuntimeEngine();
+
+		KieSession kieSession = engine.getKieSession();
+
+		Map<String, Object> params = new HashMap<String, Object>();
+		Person person = new Person();
+		person.setName(Main.USER);
+		params.put("po", person);
+
+		ProcessInstance pi = kieSession.startProcess(PROCESS_ID, params);
+		System.out.println("please check server log");
+		Thread.sleep(2000);
+		System.out.println("\n");
+
+	}
+
+	public static void humanTaskOutputWithCustomParam(RemoteRestRuntimeEngineFactory factory, String PROCESS_ID)
+			throws InterruptedException {
+
+		System.out.println("###########################################");
+		System.out.println("..testing Human Task Output with Custom Param...");
+		System.out.println("###########################################");
+
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		Person person = new Person();
+		person.setName("Oscar");
+		params.put("tVar", person);
+		//
+		RemoteRuntimeEngine engine = factory.newRuntimeEngine();
+
+		KieSession kieSession = engine.getKieSession();
+		//PROCESS_ID = "remoteIssuesProject.humanTaskOutput";
+		ProcessInstance pi = kieSession.startProcess(PROCESS_ID);
+
+		TaskService taskService = engine.getTaskService();
+		List<Long> taskList = taskService.getTasksByProcessInstanceId(pi.getId());
+
+		taskService.start(taskList.get(0), Main.USER);
+
+		taskService.complete(taskList.get(0), Main.USER, params);
+		System.out
+				.println("Ye should check the server log and see the value of the process variable, which we just filled"
+						+ " by completing human task");
+		System.out.println("\n");
+
+		Thread.sleep(2000);
+
+	}
+
+	public static void humanTaskInputWithCustomParam(RemoteRestRuntimeEngineFactory factory, String PROCESS_ID) {
+
+		System.out.println("###########################################");
+		System.out.println("...testing Human Task Input with Custom Param...");
+		System.out.println("###########################################");
+		System.out.println("we have set process variable with the object Person, which has name "+Main.USER+" set.\n"
+				+ "Then it was mapped as Task Input and it should be correctly retrieved.");
+
+		RemoteRuntimeEngine engine = factory.newRuntimeEngine();
+
+		KieSession kieSession = engine.getKieSession();
+		TaskService taskService = engine.getTaskService();
+
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		Person person = new Person();
+		person.setName(Main.USER);
+		params.put("po", person);
+
+		ProcessInstance pi = kieSession.startProcess(PROCESS_ID, params);
+
+		Task task = taskService.getTaskById(taskService.getTasksByProcessInstanceId(pi.getId()).get(0));
+		Content content = taskService.getContentById(task.getTaskData().getDocumentContentId());
+		System.out.println("Task Data:" + task.getTaskData());
+		System.out.println("Content:" + content.getContent());
+
+		Object unmarshalledObject = ContentMarshallerHelper.unmarshall(content.getContent(), null);
+		System.out.println("unmarshalledObject:" + unmarshalledObject);
+		if (!(unmarshalledObject instanceof Map)) {
+			System.err.println("well, this shouldn't really happen");
+
+		}
+		Map<String, Object> result = (Map<String, Object>) unmarshalledObject;
+		System.out.println("Result:" + result);
+		Person input = (Person) result.get("po");
+		System.out.println("task input:" + input.getName());
+
+		taskService.start(taskService.getTasksByProcessInstanceId(pi.getId()).get(0), Main.USER);
+		taskService.complete(taskService.getTasksByProcessInstanceId(pi.getId()).get(0), Main.USER, null);
+	}
+}
