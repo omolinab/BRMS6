3b1ade324e782ad91dc53d3294680c8cad4a4541
Oscar Molina <omolinab@redhat.com>
2015-01-30 17:53:56 +0100

    Adding business logic example which servlet helloworld depen

---
+++ business-logic/src/main/java/org/redhat/gss/logic/BusinessLogic.java
@@ -0,0 +1,16 @@
+package org.redhat.gss.logic;
+
+import javax.ejb.Stateless;
+
+
+@Stateless
+public class BusinessLogic implements BusinessLogicInt {
+
+	public void executeLogic() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is where business logic would be stored");
+		
+	}
+
+}
---
+++ business-logic/src/main/java/org/redhat/gss/logic/BusinessLogicInt.java
@@ -0,0 +1,7 @@
+package org.redhat.gss.logic;
+
+public interface BusinessLogicInt {
+	
+	public void executeLogic();
+
+}
