3ad3dac8c0b72d9185c0b8b367fc8ff343bdaf07
Oscar Molina <omolinab@redhat.com>
2015-02-13 13:32:52 +0100

    REST standalone client example

---
+++ jbpm-rest-standalone-client/README.md
@@ -0,0 +1,8 @@
+A standalone client using the classe 'RemoteRestRuntimeFactory' to access BPM tasks.
+
+To execute the client, you must have Maven installed. Then you just need to type the following command:
+
+$ mvn package exec:java -Dexec.mainClass="com.redhat.gss.Main
+
+
+
---
+++ jbpm-rest-standalone-client/src/main/java/com/redhat/gss/Main.java
@@ -0,0 +1,54 @@
+package com.redhat.gss;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import org.kie.api.task.TaskService;
+import org.kie.api.task.model.TaskSummary;
+import org.kie.services.client.api.RemoteRestRuntimeFactory;
+import org.kie.services.client.api.command.RemoteRuntimeEngine;
+
+/**
+ * This is a very simple Rest Client to test against a running
+ *  instance of the KIE Workbench. 
+ * You can parameterize
+ *   - the Deployment Unit Id
+ *   - the Application URL
+ *   - the user/pass to execute operations
+ */
+public class Main {
+
+	/*
+		Set the parameters accoring your installation	 
+	*/
+	private static final String DEPLOYMENT_ID = "org.redhat:sampleProject:1.0";
+	//private static final String APP_URL = "http://localhost:8080/business-central/";
+	private static final String APP_URL = "http://192.168.1.101:8080/business-central/";
+	private static final String USER = "erics";
+	private static final String PASSWORD = "bpmsuite";
+
+	public static void main( String[] args ) throws MalformedURLException {
+		URL url = new URL(APP_URL);
+		// create a factory using the installation parameters
+		RemoteRestRuntimeFactory factory = new RemoteRestRuntimeFactory(DEPLOYMENT_ID, url, USER, PASSWORD);
+		RemoteRuntimeEngine engine = factory.newRuntimeEngine();
+		// The TaskService class allows we to access the server tasks
+		TaskService taskService = engine.getTaskService();
+		List<TaskSummary> tasks = taskService.getTasksOwned(USER, "en-UK");
+		if(tasks.size() == 0){
+			System.out.printf("No tasks for user \"%s\" as owner...\n", USER);
+		} else {
+			System.out.printf("Tasks where user \"%s\" is a an owner...\n", USER);
+			for(TaskSummary t : tasks){
+				System.out.printf("ID: %d\n", t.getId());
+				System.out.printf("Name: %s\n", t.getName());
+				System.out.printf("Actual Owner: %s\n", t.getActualOwner());
+				System.out.printf("Created by: %s\n", t.getCreatedBy());
+				System.out.printf("Created on: %s\n", t.getCreatedOn());
+				System.out.printf("Status: %s\n", t.getStatus());
+				System.out.printf("Description: %s\n", t.getDescription());
+				System.out.println("---------------");
+			}
+		}
+	}   
+}
